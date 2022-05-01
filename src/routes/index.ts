3a5580fdff720e56408648597621e505b4250dd0
Cat Cat <67152874+catcat1204@users.noreply.github.com>
2022-05-02 00:00:22 +0700

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import typeRouter from "./v2";
-import ioeRouter from "./ioe";
 import adminRoute from "./admin";
 const router = Router();
 router.use("/v2", typeRouter);
-router.use("/ioe", ioeRouter);
 router.use("/admin", adminRoute);
 router.get("/", (req, res) => {
   res.redirect("https://docs.huyapi.ga/");
